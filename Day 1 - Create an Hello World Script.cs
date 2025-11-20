197e318ad1fda57f5fbffdbc7eb7ca208a866bb4
Luigi Daniele <93317421+lonluda@users.noreply.github.com>
2025-11-21 00:17:56 +0100

    A Simple script to output Hello World message
    Hello World message.

---
+++ Day 1 - Create an Hello World Script.cs
@@ -0,0 +1,14 @@
+using System;
+
+namespace HelloWorld
+{
+    class HelloWorld
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World !");
+            Console.WriteLine("This was generated from a C# Script.");
+        }
+
+    }
+}
