fc1f0ea265c4eb42326c8f6ee29ba49e8433ff9d
Luigi Daniele <93317421+lonluda@users.noreply.github.com>
2025-11-23 12:12:44 +0100

    Refactor HelloWorld class and Main method

--- Day 1 - Create an Hello World Script.cs
+++ Day 1 - Create an Hello World Script.cs
@@ -1,14 +1,13 @@
 using System;
 
-namespace HelloWorld
-{
-    class HelloWorld
-    {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World !");
-            Console.WriteLine("This was generated from a C# Script.");
-        }
+namespace HelloWorld;
 
+class HelloWorld
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello World !");
+        Console.WriteLine("This was generated from a C# Script.");
     }
+
 }
