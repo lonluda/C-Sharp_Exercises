dafc35a7d121e68d7599a1a7930a37a9f8d9fda8
Luigi Daniele <93317421+lonluda@users.noreply.github.com>
2025-11-26 21:13:25 +0100

    Add program to print even numbers from 0 to 9

---
+++ Day 9 - Even number.cs
@@ -0,0 +1,18 @@
+using System;
+
+class App
+{
+    public static void Main()
+    {
+
+    for(int i = 0; i < 10; i++)
+        {
+            if(i % 2 == 0)
+            {
+                Console.WriteLine(i);
+            }
+            
+        }
+
+    }
+}
