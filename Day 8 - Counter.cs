3e05a7ed579fb9fec8b0fdeec93054b67f4a8af9
Luigi Daniele <93317421+lonluda@users.noreply.github.com>
2025-11-26 21:07:16 +0100

    Add Counter program to print numbers 0 to 10

---
+++ Day 8 - Counter.cs
@@ -0,0 +1,17 @@
+using System;
+
+class App
+{
+    public static void Main()
+    {
+
+        int i = 0;
+
+        while(i < 11)
+        {
+            Console.WriteLine(i);
+            i++;
+        }
+
+    }
+}
